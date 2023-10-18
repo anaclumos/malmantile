e9a22d18f7c7f39e51ce1a2f6a617ced7faea4da
Sunghyun Cho <hey@cho.sh>
2023-10-17 23:07:24 -0700

    feat: Add Control.py with ResNet model training and evaluation

---
+++ Control.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import datetime
+
+# Constants
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+NUM_EPOCHS = 100
+LEARNING_RATE = 0.0001
+BATCH_SIZE = 10
+
+
+def get_transforms():
+    return transforms.Compose(
+        [
+            transforms.Pad(4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32),
+            transforms.ToTensor(),
+        ]
+    )
+
+
+def get_dataloaders():
+    transform = get_transforms()
+
+    train_dataset = torchvision.datasets.CIFAR10(
+        root="./control", train=True, transform=transform, download=True
+    )
+    test_dataset = torchvision.datasets.CIFAR10(
+        root="./control", train=False, transform=transforms.ToTensor()
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False
+    )
+
+    return train_loader, test_loader
+
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
+    )
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+# ResNet
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[1], 2)
+        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels),
+            )
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+def update_lr(optimizer, lr):
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def train_and_evaluate(model, train_loader, test_loader, optimizer, criterion):
+    accuracy_list = []
+    total_step = len(train_loader)
+    curr_lr = LEARNING_RATE
+
+    for epoch in range(NUM_EPOCHS):
+        for i, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if (epoch + 1) % 20 == 0:
+            curr_lr /= 3
+            update_lr(optimizer, curr_lr)
+
+        # Evaluate the model's accuracy on the test set after each epoch
+        model.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(
+            f"Epoch [{epoch+1}/{NUM_EPOCHS}] Accuracy of the model on the test images: {100 * correct / total} %"
+        )
+        accuracy_list.append(100 * correct / total)
+
+    return accuracy_list
+
+
+if __name__ == "__main__":
+    train_loader, test_loader = get_dataloaders()
+
+    model = ResNet(ResidualBlock, [2, 2, 2]).to(DEVICE)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    acc = train_and_evaluate(model, train_loader, test_loader, optimizer, criterion)
+
+    # plot the accuracy
+    plt.plot(acc)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.show()
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    plt.savefig(f"control-{current_time}.png")
+
+    # Save the model checkpoint
+    torch.save(model.state_dict(), "resnet.ckpt")
